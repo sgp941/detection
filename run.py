39644d9290561f06a15236309d51f23267433f49
Govind Patwari <shreegovind@hotmail.co.uk>
2016-06-12 09:20:30 +0100

    run chnages final

--- run.py
+++ run.py
@@ -4,10 +4,20 @@ import os
 from subprocess import call
 
 dirs = os.listdir(".")
+dirs = [x[0] for x in os.walk(".") if "git" not in x[0]]
+dirs.pop(0)
+print dirs
 
 configFileName = "config.txt"
 
-for dir in dirs:
+totalDirs = len(dirs)
+
+
+for (count, dir) in enumerate(dirs):
+	print "=============================================================================="
+	print "WE ARE ON FILE " + str(count) + " OF " + str(totalDirs)
+ 	print "=============================================================================="
+
 	os.chdir(dir)
 	print call("pwd")
 	call(["../HoughForests", "0", configFileName])
